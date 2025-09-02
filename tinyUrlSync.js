056fd350be080c51ea8319427498a290ad6f8495
signothecat <shinogoroneko0818@gmail.com>
2025-09-03 00:25:31 +0900

    🐞 fix: reset url when reloading

--- tinyUrlSync.js
+++ tinyUrlSync.js
@@ -2,8 +2,9 @@ const textInput = document.getElementById("textInput");
 
 // On page load, reset URL and input to initial state
 window.addEventListener("DOMContentLoaded", () => {
-  history.replaceState(null, null, "/");
   textInput.value = "";
+  const path = "/" + encodeURIComponent(textInput.value);
+  history.replaceState(null, null, path);
 });
 
 // Update the URL path whenever the input changes
